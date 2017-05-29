d3720a2a2736d4ee4677aab97d9cf231b4f96dc2
david <lexuszhi1990@gmail.com>
2017-05-29 14:14:06 +0800

    update

--- src/evaluation_mat/eval_dataset.m
+++ src/evaluation_mat/eval_dataset.m
@@ -1,10 +1,10 @@
 function [PSNR, SSIM, IFC] = eval_dataset(dataset_dir, sr_method, sr_factor)
 
 % usage:
-% eval_dataset('../../dataset/test/set5', 'lapsrn', 4)
+% eval_dataset('../../dataset/test/set14', 'bicubic', 4)
 
 addpath('./matlabPyrTools/');
-addpath('./IFC/');
+addpath('./ifc-drrn/');
 
 dataset_gt_path = fullfile(dataset_dir, 'GT');
 dataset_sr_path = fullfile(dataset_dir, sr_method);
@@ -27,30 +27,25 @@ for n=1:numel(gt_lst)
     return ;
   end
 
+  % v2: DRRN
   gt_img = imread(gt_img_path);
+  gt_img_ycbcr = rgb2ycbcr(gt_img);
+  gt_img_y = im2double(gt_img_ycbcr(:,:,1));
+  gt_img_y_shaved = shave(uint8(single(gt_img_y) * 255), [sr_factor, sr_factor]);
+
   generated_img = imread(generated_img_path);
-
-  if size(gt_img,3) == 1
-    gt_img = cat(3, gt_img, gt_img, gt_img);
-  end
-  gt_img_ycbcr = rgb2ycbcr(im2double(gt_img));
-  gt_img_y = gt_img_ycbcr(:,:,1);
-  gt_img_y_shaved = shave(gt_img_y, [sr_factor, sr_factor]);
-
-  if size(generated_img,3) == 1
-    generated_img = cat(3, generated_img, generated_img, generated_img);
-  end
-  generated_img_ycbcr = rgb2ycbcr(im2double(generated_img));
-  generated_img_y = generated_img_ycbcr(:,:,1);
-  generated_img_y_shaved = shave(generated_img_y, [sr_factor, sr_factor]);
+  % generated_img = imresize(imresize(gt_img, 1.0/sr_factor, 'bicubic'), sr_factor, 'bicubic');
+  generated_img_ycbcr = rgb2ycbcr(generated_img);
+  generated_img_y = im2double(generated_img_ycbcr(:,:,1));
+  generated_img_y_shaved = shave(uint8(single(generated_img_y) * 255), [sr_factor, sr_factor]);
 
   PSNR(n) = psnr_index(gt_img_y_shaved, generated_img_y_shaved);
   SSIM(n) = ssim_index(gt_img_y_shaved, generated_img_y_shaved);
   IFC(n) = ifcvec(double(gt_img_y_shaved), double(generated_img_y_shaved));
 
-  fprintf('for image %s:\n--PSNR: %.4f\tSSIM: %.4f\tIFC: %.4f\n', gt_img_name, PSNR(n), SSIM(n), IFC(n));
+  fprintf('for image %s:\n--PSNR: %.4f;\tSSIM: %.4f;\tIFC: %.4f\n', gt_img_name, PSNR(n), SSIM(n), IFC(n));
 end
 
-fprintf('for dataset %s, upscaled by %s, at scale:%d\n--PSNR: %.4f\tSSIM: %.4f\tIFC: %.4f\n', dataset_dir, sr_method, sr_factor, mean(PSNR), mean(SSIM), mean(IFC));
+fprintf('for dataset %s, upscaled by %s, at scale:%d\n--PSNR: %.4f;\tSSIM: %.4f;\tIFC: %.4f;\n', dataset_dir, sr_method, sr_factor, mean(PSNR), mean(SSIM), mean(IFC));
 
 end
