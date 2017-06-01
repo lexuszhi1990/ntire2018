40aefba4b35e29d470720c9d75d46919555fc62f
david <lexuszhi1990@gmail.com>
2017-06-01 23:10:18 +0800

    update dataset

--- src/dataset.py
+++ src/dataset.py
@@ -2,26 +2,27 @@ import numpy as np
 import h5py
 import cv2
 
-class Dataset(object):
-  def __init__(self, path, batch_size=10, upscale=4):
-    self.path = path
+class TrainDataset(object):
+  def __init__(self, file_path, train_img_size=[128,128], batch_size=10, upscale=4):
+    self.file_path = file_path
     self.batch_size = batch_size
     self.upscale = upscale
 
-    self.hf = h5py.File(self.path)
+    self.hf = h5py.File(self.file_path)
     self.data = self.hf.get('data')
     self.label = self.hf.get('label')
     self.len = self.data.len()
+    _, self.channel, self.max_height, self.max_width = np.shape(self.data)
+    self.gt_height, self.gt_width = train_img_size
     self.batch_ids = self.data.len() // self.batch_size
 
-    _, self.channel, self.gt_height, self.gt_width = np.shape(self.data)
-    self.input_image_size = [self.gt_height/self.upscale, self.gt_width/self.upscale]
+    self.input_image_size = [self.gt_height//self.upscale, self.gt_width//self.upscale]
 
   def transform(self, image):
     return np.array(image)/127.5 - 1.
 
   def batch_transpose(self,images):
-    return [image.T for image in images]
+    return np.array([image.T for image in images])
 
   def batch_resize(self, images):
     aa = [cv2.resize(image, None, fx=1.0/self.upscale, fy=1.0/self.upscale,interpolation=cv2.INTER_CUBIC) for image in images]
@@ -37,7 +38,8 @@ class Dataset(object):
     batch_label = self.batch_transpose(batch_label)
     batch_input = self.batch_resize(batch_label)
 
-    return self.transform(batch_input), self.transform(batch_label)
+    # return self.transform(batch_input), self.transform(batch_label)
+    return batch_input, batch_label
 
 class DatasetFromHdf5(object):
     def __init__(self, file_path, batch_size=8, upscale=4):
@@ -50,7 +52,6 @@ class DatasetFromHdf5(object):
         self.upscale = upscale
         self.len = self.data.len()
         self.batch_ids = self.data.len() // self.batch_size
-
         self.input_image_size = [self.data.shape[2], self.data.shape[3]]
         _, self.channel, self.gt_height, self.gt_width = np.shape(self.label_x4)
 
--- src/utils.py
+++ src/utils.py
@@ -3,17 +3,44 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import random
 
-def mse(target, base):
-  return tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(target, base))))
+def process_train_img(img, size, scale):
+  with tf.variable_scope('process_img'):
+    batch_size = tf.Tensor.get_shape(img).as_list()[0]
+    height, width, channel = size
+
+    rd = random.uniform(0.5, 1)
+    random_height, random_width = int(height*rd), int(height*rd)
+    gt_img = tf.random_crop(img, [batch_size, random_height, random_width, channel])
+    gt_img = tf.image.resize_bicubic(gt_img, [height, width], align_corners=False)
+
+    flip_lr_img = tf.split(value=gt_img, num_or_size_splits=batch_size, axis=0)
+    gt_img = tf.concat([tf.expand_dims(tf.image.random_flip_left_right(f_img[0]), axis=0) for f_img in flip_lr_img], axis=0)
+    flip_ud_img = tf.split(value=gt_img, num_or_size_splits=batch_size, axis=0)
+    gt_img = tf.concat([tf.expand_dims(tf.image.random_flip_up_down(f_img[0]), axis=0) for f_img in flip_ud_img], axis=0)
+
+    input_img = tf.image.resize_bicubic(gt_img, [height//scale, width//scale], align_corners=False)
+
+    # return gt_img, input_img
+    return trainsform(input_img), trainsform(gt_img)
+
+def trainsform(img):
+  trains_img = tf.divide(img, 255.0)
+
+  return trains_img
 
 def transform_reverse(image):
-  upscale_img = tf.divide(tf.add(image, 1.0), 2.0)
+  upscale_img = tf.multiply(image, 255.0)
+  upscale_img = tf.clip_by_value(upscale_img, 0, 255)
+  # upscale_img = tf.divide(tf.add(image, 1.0), 2.0)
   # upscale_img = tf.cast(tf.multiply(tf.add(image, 1.0), 127.5), tf.uint8)
-  # upscale_img = tf.clip_by_value(tf.multiply(upscale_img, 255.0), 0, 255)
   # upscale_img = tf.cast(tf.multiply(tf.add(image, 1.0), 127.5), tf.uint8)
 
   return upscale_img
+
+def mse(target, base):
+  return tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(target, base))))
 
 def tf_flag_setup(flags):
 
@@ -27,15 +54,17 @@ def tf_flag_setup(flags):
   flags.DEFINE_string('g_log_dir', './log/lapsrn-g', "folder to log generator train models")
 
   flags.DEFINE_integer('g_decay_steps', 100, "decay learning rate by intervel steps")
-  flags.DEFINE_float('g_decay_rate', 0.1, "decay rate by intervel steps")
+  flags.DEFINE_float('g_decay_rate', 0.9, "decay rate by intervel steps")
   flags.DEFINE_float('lr', 1e-4, "g learning rate")
 
-  flags.DEFINE_integer('channel', 2, "data input channel")
   flags.DEFINE_integer('upscale_factor', 4, "upscale factor")
 
   flags.DEFINE_bool('is_training_mode', True, "whether continued training from last ckpt")
   flags.DEFINE_bool('continued_training', False, "whether continued training from last ckpt")
   flags.DEFINE_bool('debug', False, "whether or not to print debug messages")
+
+  # Sets the graph-level random seed.
+  tf.set_random_seed(1234)
 
 def setup_project(FLAGS):
   # init dirs
