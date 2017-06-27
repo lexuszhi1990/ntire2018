4ea8c23a369636a137fd0a15b03065a92659275f
david <lexuszhi1990@gmail.com>
2017-06-27 11:15:06 +0800

    update v2 model

--- src/model.py
+++ src/model.py
@@ -161,7 +161,7 @@ class LapSRN_v2(object):
   def __init__(self, inputs, gt_img_x2, gt_img_x4, gt_img_x8, image_size, is_training, upscale_factor=4, filter_num=64, reg=1e-4, scope='lap_ml_srn'):
     self.scope = scope
     self.inputs = inputs
-    self.gt_imgs = [gt_img_x2, gt_img_x4, gt_img_x8]
+    self.labels = vars()["gt_img_x{}".format(upscale_factor)]
     self.height = np.uint32(image_size[0])
     self.width = np.uint32(image_size[1])
 
@@ -174,6 +174,7 @@ class LapSRN_v2(object):
     self.kernel_size = 3
     self.batch_size, _, _, self.channel = tf.Tensor.get_shape(inputs).as_list()
 
+    self.gt_imgs = []
     self.sr_imgs = []
     self.reconstructed_imgs = []
     self.extracted_features = []
@@ -181,6 +182,14 @@ class LapSRN_v2(object):
   @property
   def vars(self):
     return [var for var in tf.trainable_variables() if self.scope in var.name]
+
+  def init_gt_imgs(self):
+    for scale in range(1, self.upscale_factor):
+      height = self.height*scale
+      width = self.width*scale
+      img = tf.image.resize_bicubic(self.labels, size=[height, width], align_corners=False, name='level_{}_gt_img'.format(str(scale)))
+      self.gt_imgs.append(img)
+    self.gt_imgs.append(self.labels)
 
   def extract_features(self, reuse=False):
     with tf.variable_scope(self.scope) as vs:
@@ -192,27 +201,27 @@ class LapSRN_v2(object):
         # x = batch_normalize(x, self.is_training)
         x = lrelu(x)
 
-      for scale in range(2, self.upscale_factor+1):
+      for scale in range(1, self.upscale_factor+1):
 
         with tf.variable_scope('level_{}_img'.format(str(scale))):
           # current width and height for current stage.
-          height = self.height*scale.astype(int)
-          width = self.width*scale.astype(int)
+          height = self.height*scale
+          width = self.width*scale
           x = tf.image.resize_bilinear(x, size=[height, width], align_corners=False, name='level_{}_transpose_upscale'.format(str(scale)))
           # x = layers.conv2d_transpose(x, self.filter_num, 4, stride=2, padding='SAME', activation_fn=lrelu, biases_initializer=None, , weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='level_{}__transpose_upscale'.format(str(l)))
           # x = deconv_layer(x, [3, 3, self.filter_num, self.filter_num], [self.batch_size, height, width, self.filter_num], stride=1)
 
         for d in range(self.residual_depth):
-          x = layers.conv2d(x, self.filter_num, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer = layers.l2_regularizer(scale=self.reg), scope='level_{}_residual_{}'.format(str(scale), str(d)))
+          x = layers.conv2d(x, self.filter_num, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='level_{}_residual_{}'.format(str(scale), str(d)))
 
-        net = layers.conv2d(x, 1, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='level_{}_img'.format(str(scale)))
+        net = layers.conv2d(x, 1, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=None, biases_initializer=None, weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='level_{}_img'.format(str(scale)))
 
         self.extracted_features.append(net)
 
       base_images = self.inputs
-      for scale in range(2, self.upscale_factor+1):
-        upscaled_width = self.width*scale.astype(int)
-        upscaled_height = self.height*scale.astype(int)
+      for scale in range(1, self.upscale_factor+1):
+        upscaled_width = self.width*scale
+        upscaled_height = self.height*scale
         base_images = tf.image.resize_bilinear(base_images, size=[upscaled_height, upscaled_width], align_corners=False, name='level_{}_biliear'.format(str(scale)))
 
         # base_images = layers.conv2d_transpose(base_images, 1, 4, stride=2, padding='SAME', activation_fn=lrelu, biases_initializer=None, scope='level_{}_img_upscale_transpose'.format(str(l)))
@@ -225,13 +234,16 @@ class LapSRN_v2(object):
       if reuse:
         vs.reuse_variables()
 
-      for l in range(self.upscale_factor-1):
+      for l in range(self.upscale_factor):
         sr_img = tf.add(self.reconstructed_imgs[l], self.extracted_features[l])
         self.sr_imgs.append(sr_img)
 
   def l1_loss(self):
+    if len(model.gt_imgs) == 0:
+      self.init_gt_imgs()
+
     loss = 0.0
-    for l in range(self.level):
+    for l in range(self.upscale_factor):
       loss = loss + self.l1_charbonnier_loss(self.sr_imgs[l], self.gt_imgs[l])
 
     return loss
