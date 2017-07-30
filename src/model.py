698baa234b93ff4bb419aabc41f6d1869788a257
your_name <your_email>
2017-07-30 15:43:47 +0800

    add v1 model

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from src.layer import *
 import tensorflow.contrib.layers as layers
 
-class LapSRN_v1(object):
+class LapSRN_v1_bak(object):
   def __init__(self, inputs, gt_img_x2, gt_img_x4, gt_img_x8, image_size, is_training, upscale_factor=4, filter_num=64, reg=1e-4, scope='lap_srn'):
     self.scope = scope
     self.inputs = inputs
@@ -161,6 +161,102 @@ class LapSRN_v1(object):
 
   def upscaled_img(self, index):
     return self.sr_imgs[np.log2(index).astype(int)-1]
+
+
+class LapSRN_v1(object):
+  def __init__(self, inputs, gt_img_x2, gt_img_x4, gt_img_x8, image_size, is_training, upscale_factor=4, filter_num=64, reg=1e-4, scope='lap_srn'):
+    self.scope = scope
+    self.inputs = inputs
+    self.gt_imgs = [gt_img_x2, gt_img_x4, gt_img_x8]
+    self.height = np.uint32(image_size[0])
+    self.width = np.uint32(image_size[1])
+
+    self.upscale_factor = upscale_factor
+    self.level = np.log2(upscale_factor).astype(int)
+    self.filter_num = filter_num
+    self.reg = reg
+    self.is_training = is_training
+    self.residual_depth = 10
+    self.kernel_size = 3
+    self.image_squeeze_channle = 256
+    self.batch_size, _, _, self.channel = tf.Tensor.get_shape(inputs).as_list()
+
+    self.sr_imgs = []
+    self.reconstructed_imgs = []
+    self.extracted_features = []
+
+  @property
+  def vars(self):
+    return [var for var in tf.trainable_variables() if self.scope in var.name]
+
+  def init_gt_imgs(self):
+    pass
+
+  def extract_features(self, reuse=False):
+    with tf.variable_scope(self.scope) as vs:
+      if reuse:
+        vs.reuse_variables()
+
+      bilinear_weights_init = tf.constant_initializer(value=bilinear_upsample_weights(2, self.filter_num), dtype=tf.float32)
+
+      x = layers.conv2d(self.inputs, self.filter_num, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='init')
+
+      for l in range(self.level):
+        for d in range(self.residual_depth):
+          x = layers.conv2d(x, self.filter_num, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer = layers.l2_regularizer(scale=self.reg), scope='level_{}_residual_{}'.format(str(l), str(d)))
+
+        # current upscaled width and height for current stage.
+        x = layers.conv2d_transpose(x, self.filter_num, 4, stride=2, padding='SAME', activation_fn=lrelu, weights_initializer=bilinear_weights_init, biases_initializer=None, scope='level_{}__transpose_upscale'.format(str(l)))
+
+
+        net = layers.conv2d(x, 1, kernel_size=self.kernel_size, stride=1, padding='SAME', activation_fn=lrelu, biases_initializer=None, weights_regularizer=layers.l2_regularizer(scale=self.reg), scope='level_{}_img'.format(str(l)))
+
+        self.extracted_features.append(net)
+
+      base_images = self.inputs
+      for l in range(self.level):
+        base_images = layers.conv2d_transpose(base_images, 1, 4, stride=2, padding='SAME', activation_fn=lrelu, weights_initializer=bilinear_weights_init, biases_initializer=None, scope='level_{}_img_upscale_transpose'.format(str(l)))
+
+        self.reconstructed_imgs.append(base_images)
+
+  def reconstruct(self, reuse=False):
+    with tf.variable_scope(self.scope) as vs:
+      if reuse:
+        vs.reuse_variables()
+
+      for l in range(self.level):
+        sr_img = tf.add(self.reconstructed_imgs[l], self.extracted_features[l])
+        self.sr_imgs.append(sr_img)
+
+  def l1_loss(self):
+    loss = []
+    for l in range(self.level):
+      loss.append(self.l1_charbonnier_loss(self.sr_imgs[l], self.gt_imgs[l]))
+
+    loss = tf.reduce_sum(loss)
+
+    return loss
+
+  def l1_charbonnier_loss(self, X, Y):
+    eps = 1e-6
+    diff = tf.subtract(X, Y)
+    error = tf.sqrt( diff * diff + eps)
+    loss  = tf.reduce_mean(error)
+
+    return loss
+
+  def l2_loss(self):
+    # diff = (X - Z) .^ 2;
+    # Y = 0.5 * sum(diff(:));
+    loss = []
+    for l in range(self.level):
+      diff = tf.square(tf.subtract(self.sr_imgs[l], self.gt_imgs[l]))
+      loss.append(0.5 * tf.reduce_mean(diff))
+
+    return tf.reduce_sum(loss)
+
+  def upscaled_img(self, index):
+    return self.sr_imgs[-1]
 
 class LapSRN_v2(object):
   def __init__(self, inputs, gt_img_x2, gt_img_x4, gt_img_x8, image_size, is_training, upscale_factor=4, filter_num=64, reg=1e-4, scope='lap_ml_srn'):
